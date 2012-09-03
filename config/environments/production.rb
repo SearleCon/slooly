4d29c119992d077ba7ed46ebf2304caf4773b869
Searle Consulting <shaun@searleconsulting.co.za>
2012-09-03 10:56:50 +0200

    Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Slooly::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'http://glacial-taiga-1918.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -72,11 +72,11 @@ Slooly::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "http://glacial-taiga-1918.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "searlecon@gmail.com",
+    password: "argitdyzrctfidai"
   }
 
 
