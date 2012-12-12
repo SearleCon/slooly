2f358dff303e1b5c84789f840ef7370842dd5776
Searle Consulting <shaun@searleconsulting.co.za>
2012-12-12 10:30:03 +0200

    Fixed the vouchers look and feel

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -2,9 +2,9 @@ class VouchersController < ApplicationController
   def redeem
     respond_to do |format|
       if Voucher.redeemable?(params[:unique_code]) && Voucher.redeem(params[:unique_code], current_user.id)
-        format.html { redirect_to edit_user_registration_path, notice: 'Voucher redeemed successfully' }
+        format.html { redirect_to edit_user_registration_path, notice: 'Congratulations! Your voucher was successfully redeemed!' }
       else
-        format.html { redirect_to edit_user_registration_path, alert: 'An error occured while trying to redeem your voucher. If this continues, please contact support.' }
+        format.html { redirect_to edit_user_registration_path, alert: 'Unable to redeem the voucher. Please make sure you typed it correctly, and it is still valid. If this error persists, please contact support.' }
       end
     end
     
