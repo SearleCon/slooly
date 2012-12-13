24a23572957a3e7ea04999d353d0f8f026f47575
Searle Consulting <shaun@searleconsulting.co.za>
2012-12-13 14:48:34 +0200

    Live Paypal setup - Changed the Use_Sandbox to False in paypal.rb

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,7 +1,7 @@
 require "paypal/recurring"
 
 PayPal::Recurring.configure do |config|
-  config.sandbox = true
+  config.sandbox = false
 #  config.sandbox = ENV["PAYPAL_USE_SANDBOX"]
   config.username = ENV["PAYPAL_USERNAME"]
   config.password = ENV["PAYPAL_PASSWORD"]
