e56abbac94f42106be0261f8c6ef0126afc1b125
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-06-06 16:10:31 +0200

    Added honeypot to contact form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,21 +1,19 @@
 class ContactController < ApplicationController
   
-  def new
+    def new
       @message = Message.new
     end
 
     def create
-      @message = Message.new(params[:message])
-
-      if @message.valid?
-#        UserMailer.delay.new_message(@message) # SS You removed this due to it not working on Delayed Job - Syck error. You added the next line instead so it doesnt use DelayedJob
-        
-        UserMailer.new_message(@message).deliver
-        redirect_to(root_path, :notice => "Message was successfully sent.")
-      else
-        flash.now.alert = "Please fill all fields."
-        render :new
-      end
+        @message = Message.new(params[:message])
+        if @message.valid?
+          unless params[:content].present?
+            UserMailer.new_message(@message).deliver
+            flash[:notice] = "Message was successfully sent."
+          end
+          redirect_to root_url
+        else
+          render :new
+        end
     end
-    
 end
