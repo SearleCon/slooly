feafae4c166db1642565b58c3eae6889ba90c29a
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2015-11-11 17:55:03 +0200

    Fixed welcome page path in sign up path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(_resource)
-    welcome_path
+    welcome_index_path
   end
 
   def sign_up_params
