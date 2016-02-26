1e085a808074cf6d26b6cc9dc54e57347da006d8
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2016-02-26 13:56:44 +0200

    Fixed contacts email serialization problem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @message = Message.new(params[:message])
     if @message.valid?
-      UserMailer.new_message(@message).deliver_later
+      UserMailer.new_message(@message).deliver_now
       redirect_to root_url, notice: 'Message was successfully sent.'
     else
       render :new
